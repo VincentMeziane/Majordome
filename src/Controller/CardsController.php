b51f0e26cf38e5abe17a02f114a984aeafbc07ed
Vincent <mezianev@hotmail.fr>
2020-09-20 16:58:43 +0200

    Added show template and show method to display one card at a time

--- src/Controller/CardsController.php
+++ src/Controller/CardsController.php
@@ -2,7 +2,9 @@
 
 namespace App\Controller;
 
+use App\Entity\Card;
 use App\Repository\CardRepository;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
@@ -11,9 +13,17 @@ class CardsController extends AbstractController
     /**
      * @Route("/", name="app_home")
      */
-    public function index(CardRepository $cardRepository )
+    public function index(CardRepository $cardRepository ): Response
     {
         $cards = $cardRepository->findAll();
         return $this->render('cards/index.html.twig', compact('cards'));
+    }
+
+    /**
+     * @Route("/card/{id<[0-9]+>}", name="app_cards_show")
+     */
+    public function show(Card $card): Response
+    {
+        return $this->render('cards/show.html.twig', compact('card'));
     }
 }
