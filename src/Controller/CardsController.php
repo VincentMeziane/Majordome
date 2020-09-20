b31c968aad9b60275088f26d14c05388f6f7c8b6
Vincent <mezianev@hotmail.fr>
2020-09-20 20:39:39 +0200

    Add create/edit form and basic navigation

--- src/Controller/CardsController.php
+++ src/Controller/CardsController.php
@@ -4,26 +4,75 @@ namespace App\Controller;
 
 use App\Entity\Card;
 use App\Repository\CardRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class CardsController extends AbstractController
 {
     /**
-     * @Route("/", name="app_home")
+     * @Route("/", name="app_home", methods="GET")
      */
-    public function index(CardRepository $cardRepository ): Response
+    public function index(CardRepository $cardRepository): Response
     {
         $cards = $cardRepository->findBy([], ['createdAt' => 'DESC']);
         return $this->render('cards/index.html.twig', compact('cards'));
     }
 
     /**
-     * @Route("/card/{id<[0-9]+>}", name="app_cards_show")
+     * @Route("/card/{id<[0-9]+>}", name="app_card_show", methods="GET")
      */
     public function show(Card $card): Response
     {
         return $this->render('cards/show.html.twig', compact('card'));
+    }
+
+    /**
+     * @Route("/card/create", name="app_card_create" , methods="GET|POST")
+     */
+    public function create(Request $request, EntityManagerInterface $em): Response
+    {
+        $card = new Card;
+        $form = $this->createFormBuilder($card)
+            ->add('title')
+            ->add('description')
+            ->getForm();
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em->persist($card);
+            $em->flush();
+            return $this->redirectToRoute('app_home');
+        } else {
+            return $this->render('cards/create.html.twig', ['formulaire' => $form->createView()]);
+        }
+    }
+    /**
+     * @Route("/card/edit/{id<[0-9]+>}", name="app_card_edit" , methods="GET|POST")
+     */
+    public function edit(Card $card,Request $request, EntityManagerInterface $em ): Response
+    {
+        $form = $this->createFormBuilder($card)
+            ->add('title')
+            ->add('description')
+            ->getForm();
+
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em->flush();
+
+            return $this->redirectToRoute('app_home');
+        }
+
+
+        return $this->render('cards/edit.html.twig', [
+            'formulaire' => $form->createView(),
+            'card' => $card
+        ]);
     }
 }
