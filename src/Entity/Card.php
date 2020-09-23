dffb1d112a5589d14816f100fa4273b71dcd42ef
Vincent <mezianev@hotmail.fr>
2020-09-23 14:47:11 +0200

    ajout du bundle vich uploader d'image et configuration

--- src/Entity/Card.php
+++ src/Entity/Card.php
@@ -6,11 +6,14 @@ use App\Entity\Traits\Timestampable;
 use App\Repository\CardRepository;
 use Doctrine\ORM\Mapping as ORM;
 use Symfony\Component\Validator\Constraints as Assert;
+use Symfony\Component\HttpFoundation\File\File;
+use Vich\UploaderBundle\Mapping\Annotation as Vich;
 
 /**
  * @ORM\Entity(repositoryClass=CardRepository::class)
  * @ORM\Table(name="cards")
  * @ORM\HasLifecycleCallbacks
+ * @Vich\Uploadable
  */
 class Card
 {
@@ -41,7 +44,14 @@ class Card
      */
     private $imageName;
 
-
+    /**
+     * NOTE: This is not a mapped field of entity metadata, just a simple property.
+     * 
+     * @Vich\UploadableField(mapping="card_image", fileNameProperty="imageName")
+     * 
+     * @var File|null
+     */
+    private $imageFile;
 
     public function getId(): ?int
     {
@@ -84,4 +94,22 @@ class Card
         return $this;
     }
 
+    /**
+     * @param File|\Symfony\Component\HttpFoundation\File\UploadedFile|null $imageFile
+     */
+    public function setImageFile(?File $imageFile = null): void
+    {
+        $this->imageFile = $imageFile;
+
+        if (null !== $imageFile) {
+            // It is required that at least one field changes if you are using doctrine
+            // otherwise the event listeners won't be called and the file is lost
+            $this->setUpdatedAt(new \DateTimeImmutable());
+        }
+    }
+
+    public function getImageFile(): ?File
+    {
+        return $this->imageFile;
+    }
 }
