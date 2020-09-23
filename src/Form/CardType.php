310795f3ac761afa753fca9cef8c89446e600ee2
Vincent <mezianev@hotmail.fr>
2020-09-23 16:08:19 +0200

    Gestion de l'upload des images

--- src/Form/CardType.php
+++ src/Form/CardType.php
@@ -5,6 +5,7 @@ namespace App\Form;
 use App\Entity\Card;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Vich\UploaderBundle\Form\Type\VichImageType;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
 class CardType extends AbstractType
@@ -12,6 +13,16 @@ class CardType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
+            ->add('imageFile', VichImageType::class, [
+                'label' => "Ajouter une image ( JPG ou PNG )",
+                'required' => false,
+                'allow_delete' => true,
+                'delete_label' => "Supprimer l'image",
+                'download_label' => 'Télécharger',
+                'download_uri' => true,
+                'image_uri' => true,
+                'asset_helper' => true,
+            ])
             ->add('title')
             ->add('description')
         ;
